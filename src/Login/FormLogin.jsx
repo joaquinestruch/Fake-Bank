0552c148e12b63712c6827a9fcea12707f9d92d9
joaquin <joacoetruuu@gmail.com>
2023-04-20 20:31:52 -0300

    first test

--- src/Login/FormLogin.jsx
+++ src/Login/FormLogin.jsx
@@ -1,7 +1,36 @@
 function FormLogin({changeLogin}) {
+  
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const data = new window.FormData(event.target)
+    const username = data.get("user"); 
+    const password = data.get("password");
+
+    const reqOptions = {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify({
+        "name": username,
+        "password": password
+      })  
+    }
     
+    fetch("https://fake-bank-server-production.up.railway.app/api/users", reqOptions)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error({"error": error})
+    })
+
+    }
+  
   return (
-    <form action="submit" className="form-login">
+    <form action="submit" className="form-login" onSubmit={(e) => {handleSubmit(e)}}>
       <input id="user" name="user" type="text" placeholder="Usuario"/>
       <input id="password" name="password" type="password" placeholder="Contraseña" />
       <button type="submit">
